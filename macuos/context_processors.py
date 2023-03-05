7eb984a60a157dcf0278408493824250da31e80b
unisia-se <s-hama@unisia-se.jp>
2023-03-05 20:05:52 +0900

    feat: 新規作成

---
+++ macuos/context_processors.py
@@ -0,0 +1,10 @@
+﻿
+from.models import BigCategory, Tag
+ 
+ 
+def common(request):
+    context = {
+        "big_categories": BigCategory.objects.all(),
+        "tags": Tag.objects.all(),
+    }
+    return context
